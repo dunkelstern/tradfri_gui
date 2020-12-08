b848b67884ccd492b41aa25bde1a92df2922d808
Johannes Schriewer <hallo@dunkelstern.de>
2020-12-08 14:32:57 +0100

    Bugfix: Copy pytradfri VERSION file to installer dir

--- build.ps1
+++ build.ps1
@@ -2,5 +2,7 @@ $env:PATH="$env:PATH;C:\Windows\System32\downlevel;C:\Program Files (x86)\NSIS"
 pyinstaller --name="TradfriGUI" --windowed --onedir --noupx -y src\main.py
 Copy-Item "bin\*.dll" ".\dist\TradfriGUI\"
 Copy-Item "bin\*.exe" ".\dist\TradfriGUI\"
+New-Item -Path ".\dist\TradfriGUI\" -Name "pytradfri" -ItemType "directory" -Force
+Copy-Item "$env:VIRTUAL_ENV\Lib\site-packages\pytradfri\VERSION" ".\dist\TradfriGUI\pytradfri\"
 Copy-Item -Recurse "icons" ".\dist\TradfriGUI\"
 makensis /X"SetCompressor /FINAL lzma" installer.nsis
